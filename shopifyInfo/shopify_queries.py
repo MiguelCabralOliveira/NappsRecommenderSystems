e94dc65e0e9ff6253c2e66b6d9b8a6e309b1230c
Miguel Oliveira <1200874@isep.ipp.pt>
2025-02-24 18:25:14 +0000

    fix : shopify query metafield fixed

--- shopifyInfo/shopify_queries.py
+++ shopifyInfo/shopify_queries.py
@@ -1,9 +1,19 @@
 import requests
 import pandas as pd
+from preprocessing.metafields_processor import get_metafield_keys, process_metafields
 
-# Fetch all collections and products using shopify storefront api
-# Currently fetching : Collections, Products, Variants, Options, Tags
-def get_collections_products(store_url: str, access_token: str, collections_limit: int = 2, products_limit: int = 2) -> pd.DataFrame | None:
+def get_collections_products(store_url: str, access_token: str, metafields: list, collections_limit: int = 100, products_limit: int = 50) -> pd.DataFrame | None:
+    """
+    Fetch collections and products data from Shopify API
+    Args:
+        store_url: Shopify store URL
+        access_token: Shopify access token
+        metafields: List of metafield configurations
+        collections_limit: Maximum number of collections to fetch
+        products_limit: Maximum number of products per collection
+    Returns:
+        DataFrame: Raw products data or None if error occurs
+    """
     url = f"{store_url}/api/2024-01/graphql.json"
     
     headers = {
@@ -11,56 +21,61 @@ def get_collections_products(store_url: str, access_token: str, collections_limi
         "Content-Type": "application/json",
     }
     
-    query = """
-    query {
-        collections(first: $collections) {
-            edges {
-                node {
+    metafields_identifiers = ",".join([
+        f'{{ key: "{field["key"]}", namespace: "{field["namespace"]}" }}'
+        for field in metafields
+    ])
+    
+    query = f"""
+    query {{
+        collections(first: $collections) {{
+            edges {{
+                node {{
                     id
                     title
-                    products(first: $products) {
-                        edges {
-                            node {
+                    products(first: $products) {{
+                        edges {{
+                            node {{
                                 id
                                 description
                                 handle
                                 title
                                 productType
                                 tags
-                                variants(first: 10) {
-                                    edges {
-                                    node {
-                                        barcode
-                                        availableForSale
-                                        compareAtPrice {
-                                        amount
-                                        }
-                                        price {
-                                        amount
-                                        }
-                                        quantityAvailable
-                                        selectedOptions {
-                                        name
-                                        }
-                                        title
-                                        unitPrice {
-                                        amount
-                                        }
-                                        taxable
-                                    }
-                                    }
-                                }
-                                options(first: 10) {
-                                    name
-                                    values
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+                                vendor
+                                metafields(identifiers: [{metafields_identifiers}]) {{
+                                    value
+                                }}
+                                variants(first: 10) {{
+                                    edges {{
+                                        node {{
+                                            barcode
+                                            availableForSale
+                                            compareAtPrice {{
+                                                amount
+                                            }}
+                                            price {{
+                                                amount
+                                            }}
+                                            quantityAvailable
+                                            selectedOptions {{
+                                                name
+                                            }}
+                                            title
+                                            unitPrice {{
+                                                amount
+                                            }}
+                                            taxable
+                                        }}
+                                    }}
+                                }}
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        }}
+    }}
     """.replace("$collections", str(collections_limit)).replace("$products", str(products_limit))
     
     try:
@@ -68,30 +83,49 @@ def get_collections_products(store_url: str, access_token: str, collections_limi
         response.raise_for_status()
         data = response.json()
         
-        # Flatten the data
         flattened_data = []
         for collection_edge in data['data']['collections']['edges']:
             collection = collection_edge['node']
             collection_title = collection['title']
             for product_edge in collection['products']['edges']:
                 product = product_edge['node']
+                
                 product_data = {
                     'product_title': product['title'],
                     'collection_title': collection_title,
                     'product_id': product['id'],
+                    'vendor': product['vendor'],
                     'description': product['description'],
                     'handle': product['handle'],
                     'product_type': product['productType'],
                     'tags': product['tags'],
                     'variants': [variant['node'] for variant in product['variants']['edges']],
-                    'options': product['options']
+                    'metafields': product['metafields']
                 }
                 flattened_data.append(product_data)
         
-        # Convert to DataFrame
         df = pd.DataFrame(flattened_data)
         return df
     
     except Exception as e:
         print(f"GraphQL Error: {e}")
         return None
+
+if __name__ == "__main__":
+    store_url = "https://missusstore.myshopify.com"
+    access_token = "b7ac831b000aacb148837ca314aa1e3e"
+    collections_limit = 100
+    products_limit = 100
+    
+    result = get_collections_products(store_url, access_token, collections_limit, products_limit)
+    
+    if result is not None:
+        print("\nSuccessfully retrieved data!")
+        print(f"\nTotal products found: {len(result)}")
+        print("\nSample of the data:")
+        print(result[['product_title', 'collection_title', 'product_type']].head())
+        
+        result.to_csv("test_products_output.csv", index=False)
+        print("\nFull data saved to 'test_products_output.csv'")
+    else:
+        print("\nFailed to retrieve data.")
